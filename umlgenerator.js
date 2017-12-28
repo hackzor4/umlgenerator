7ba7ba5b404d6587910c373b08387a017db3cf58
Ianas Alexandru <alexandru.ianas@nokia.com>
2017-12-28 16:10:56 +0200

    First commit

---
+++ umlgenerator.js
@@ -0,0 +1,54 @@
+var exec = require('child_process').exec;
+var BIM = { files : {}};
+
+
+function storeAllFilesAndFunctions(error, stdout, stderr) {
+    console.log("Error: %s\nStderr: %s\nStdout: %s\n", error, stderr, stdout);
+    stdout.split("\n").slice(0, -1).forEach(function(element) {
+        console.log("Line: %s", element);
+        var array = element.split(':'),
+            file = array[0], func = array[1];
+        file1 = file.replace(/\//gi,"_").replace(/^\./gi, "").replace(/\.js$/gi, "");
+        func1 = func.slice(0, func.indexOf("(")).replace(/^function /, "");
+        //console.log("file: %s\nfunc: %s", file1, func1);
+        if (!BIM.files.hasOwnProperty(file1)) {
+            BIM.files = { file1 : [] };
+        }
+
+        BIM.files.file1.push(func1);
+        displayAllBIM();
+    });
+}
+
+function displayAllBIM() {
+    console.log(JSON.stringify(BIM, null, 4));
+}
+
+function main (){
+    if (process.argv.length <= 2) {
+        console.log("Please enter the path");
+        process.exit(-1);
+    }
+    var param = process.argv[2];
+    BIM.path = param;
+    getAllFilesAndFunctions();
+    displayAllBIM();
+}
+
+function getAllFilesAndFunctions() {
+    console.log("Checking functions in %s", BIM.path);
+    exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"^function \"", storeAllFilesAndFunctions);
+
+}
+
+main();
+
+
+
+
+
+
+
+
+
+

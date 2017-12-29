15978aef58c4be396eb0f8d6cf050aef761c5e3c
Ianas Alexandru <alexandru.ianas@nokia.com>
2017-12-29 10:33:45 +0200

    Fixed folder name bug & installed bluebird

--- umlgenerator.js
+++ umlgenerator.js
@@ -1,9 +1,10 @@
 var exec = require('child_process').exec;
+
 var date = new Date();
 var hours = date.getHours();
 var year = date.getFullYear();
-var month = date.getMonth();
-var day = date.getDay();
+var month = date.getMonth() +1;
+var day = date.getDate();
 var minutes = date.getMinutes();
 var resultsDate = year + "_" + month + "_" + day + "_" + hours + "_" + minutes;
 var BIM = { files : {}};

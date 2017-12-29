f597c39a50d24c07281a0fd65c705406eecdc23c
adanciu <adela.danciu@gmail.com>
2017-12-29 11:23:49 +0200

    add all_files and all_requires

--- umlgenerator.js
+++ umlgenerator.js
@@ -20,12 +20,35 @@ function storeAllFilesAndFunctions(error, stdout, stderr) {
 
 
         if (!BIM.files.hasOwnProperty(file1)) {
+            BIM.files[file1] = { "all_functions" : [],
+                                 "all_requires" : []
+                                };
+
+        }
+
+        BIM.files[file1].all_functions.push(func1);
+        displayAllBIM();
+        generateResults();
+    });
+    return "done";
+}
+
+function storeAllFilesAndRequires(error, stdout, stderr) {
+    console.log("Error: %s\nStderr: %s\nStdout: %s\n", error, stderr, stdout);
+    stdout.split("\n").slice(0, -1).forEach(function(element) {
+        console.log("Line: %s", element);
+        var array = element.split(':'),
+            file = array[0], func = array[1];
+        file1 = file.replace(/\//gi,"_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
+        func1 = func.slice(0, func.indexOf("(")).replace(/^function /, "");
+
+
+        if (!BIM.files.hasOwnProperty(file1)) {
+            //console.log("ERROR: strange, the file %s should already exist in the list", file1);
             BIM.files[file1] = [];
         }
 
         BIM.files[file1].push(func1);
-        //displayAllBIM();
-        // generateResults();
     });
     return "done";
 }
@@ -37,7 +60,6 @@ function displayAllBIM() {
 function generateResults(){
     console.log(resultsDate.toString());
     exec("mkdir result_" + resultsDate);
-
 }
 
 function main (){
@@ -55,11 +77,19 @@ function main (){
         // .then(storeAllFilesAndFunctions)
         .then(displayAllBIM())
         .then(generateResults());
-    }
+
+    //getAllFilesAndRequires();
+}
 
 function getAllFilesAndFunctions() {
     console.log("Checking functions in %s", BIM.path);
     return exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"^function \"", storeAllFilesAndFunctions);
+
+}
+
+function getAllFilesAndRequires() {
+    console.log("Checking functions in %s", BIM.path);
+    return exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"require(\"", storeAllFilesAndRequires);
 
 }
 

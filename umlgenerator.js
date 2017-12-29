24f1714b0eed1d518aff251f21cb3c08e86c6056
adanciu <adela.danciu@gmail.com>
2017-12-29 17:45:08 +0200

    ongoing module dependency detection

--- umlgenerator.js
+++ umlgenerator.js
@@ -22,12 +22,11 @@ function storeAllFilesAndFunctions(stdout) {
         file1 = file.replace(/\//gi,"_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
         func1 = func.slice(0, func.indexOf("(")).replace(/^function /, "");
 
-
         if (!BIM.files.hasOwnProperty(file1)) {
             BIM.files[file1] = { "all_functions" : [],
                                  "all_requires" : []
                                 };
-        }
+        };
         BIM.files[file1].all_functions.push(func1);
     });
 };
@@ -35,20 +34,118 @@ function storeAllFilesAndFunctions(stdout) {
 function storeAllFilesAndRequires(error, stdout, stderr) {
     // console.log("Error: %s\nStderr: %s\nStdout: %s\n", error, stderr, stdout);
     stdout.split("\n").slice(0, -1).forEach(function(element) {
-        // console.log("Line: %s", element);
-        var array = element.split(':'),
-            file = array[0], func = array[1];
-        file1 = file.replace(/\//gi,"_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
-        func1 = func.slice(0, func.indexOf("(")).replace(/^function /, "");
+        console.log("Line: %s", element);
+        var array = element.split(':');
 
+        file = array[0];
+        nameOfRequire = array[1] + ((array[2]===undefined)?"":array[2]);
+        file1 = file.replace(/\//gi,"_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
+
+        //example lines:
+        //var btsUri = require("@rcp/bts-uri-utils");
+        //var child_process = Promise.promisifyAll(require("child_process"));
+        //"l2PsDebugLogCtrl": require("./laOperationHandlers/l2DebugLogHandler")
+        //promisifyAll(require("node-json-rpc"));
+
+        //console.log(">>>>nameOfRequire: \"%s\", index: %d, length: %d!", nameOfRequire, nameOfRequire.indexOf("require(\""), nameOfRequire.length);
+        nameOfRequire1 = nameOfRequire.slice(nameOfRequire.indexOf("require(\"") + 9, nameOfRequire.length);
+        nameOfRequire2 = nameOfRequire1.slice(0, nameOfRequire1.indexOf("\""));
+        //console.log("File is: %s, require is: 0: %s. 1: %s, 2:%s!", file1, nameOfRequire, nameOfRequire1, nameOfRequire2);
 
         if (!BIM.files.hasOwnProperty(file1)) {
             //console.log("ERROR: strange, the file %s should already exist in the list", file1);
-            BIM.files[file1] = [];
+            BIM.files[file1] =
+            {
+                "all_functions" : [],
+                "all_requires" : [],
+                "properties" : {
+                    "file_type" : "internal"
+                }
+            };
+        };
+        var nameOfVarRequire = "?undefined?";
+        if (nameOfRequire.indexOf("var ") > -1) {
+            nameOfVarRequire = nameOfRequire.slice(nameOfRequire.indexOf("var ") + 4, nameOfRequire.indexOf(" = "));
+        } else {
+            //to do other cases
         }
 
-        BIM.files[file1].push(func1);
+        var obj = {} ;
+        obj["require"] = nameOfRequire2;
+        obj["nameOfVariable"] = nameOfVarRequire;
+
+        BIM.files[file1].all_requires.push(obj);
+
+        //check if module require is from current code (file_type = internal) or not - will be added as file)_type=external
+        verifyAndAddNewModule(nameOfRequire2);
+
     });
+    displayAllBIM();
+    return "done";
+}
+
+function verifyAndAddNewModule(module) {
+    console.log("Checking if module is new: %s", module);
+
+    if (module.indexOf("@") > -1) {
+        //project nokia external module
+        if (!BIM.files.hasOwnProperty(module)) {
+            console.log("Found 0: %s", module);
+            // add module(=file) as new
+            BIM.files[module] =
+            {
+                "all_functions" : [],
+                "all_requires" : [],
+                "properties" : {
+                    "file_type" : "nokia_external"
+                }
+            };
+        } else {
+            //nothing to do, module already added
+            console.log("Found 1: %s", module);
+            return 1;
+        }
+    } else {
+        // solve path to module as it could be something like:
+        // "./cm/localCellQueryApi" or
+        // "../../src/rcpServices/manager")
+        if (module.lastIndexOf("\.") > -1) {
+            //project local module name
+            console.log("Found 2: %s", module);
+            var moduleCompleteName = getModuleCompleteName(module);
+            if (!BIM.files.hasOwnProperty(moduleCompleteName)) {
+                BIM.files[module] =
+                {
+                    "all_functions" : [],
+                    "all_requires" : [],
+                    "properties" : {
+                        "file_type" : "type_internal"
+                    }
+                };
+            }
+        } else { //simple name - so node standard module
+            console.log("Found 3: %s", module);
+            if (!BIM.files.hasOwnProperty(module)) {
+                BIM.files[module] =
+                {
+                    "all_functions" : [],
+                    "all_requires" : [],
+                    "properties" : {
+                        "file_type" : "node_standard"
+                    }
+                };
+            }
+        }
+    }
+
+}
+function getModuleCompleteName(module) {
+    console.log ("getModuleCompleteName for %s", module);
+
+    Object.keys(BIM.files).filter(function(el) {
+        var result=/module$/.test(BIM.files[el]) ;
+        return result;
+    }).length>0? console.log(">>> FOUND"): console.log("... not FOUND");
 }
 
 function displayAllBIM() {
@@ -56,8 +153,28 @@ function displayAllBIM() {
 }
 
 function generateResults(){
+    console.log("Generating results ....");
     console.log(resultsDate.toString());
     exec("mkdir result_" + resultsDate);
+}
+
+function main (){
+    if (process.argv.length <= 2) {
+        console.log("Please enter the path");
+        process.exit(-1);
+    }
+    var param = process.argv[2];
+    BIM.path = param;
+
+    return new Promise(function(resolve, reject){
+            resolve('Success');
+            return getAllFilesAndRequires();
+    })
+        // .then(storeAllFilesAndFunctions)
+        .then(displayAllBIM())
+        .then(generateResults());
+
+    //getAllFilesAndRequires();
 }
 
 function getAllFilesAndFunctions() {

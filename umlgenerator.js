b308ee0f107451060c32183ed06540fbdd2eda81
Adela Danciu <adela.danciu@nokia.com>
2018-04-18 18:24:00 +0300

    correction for relative path and buffer size

--- umlgenerator.js
+++ umlgenerator.js
@@ -14,19 +14,22 @@ var folder_name = resultsDate.toString();
 
 var BIM = { files : {},
 };
+function cleanPath(file) {
+     return file.replace(/\//gi,"_").replace(/\.\._/gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
+}
 
 function addNewFileModule(file, type) {
-	console.log("Add new file module: %s %s", file, type);
+    console.log("Add new file module: %s %s", file, type);
     if (!BIM.files.hasOwnProperty(file)) {
         BIM.files[file] =
-        {
-            "all_functions" : [],
-            "all_requires" : [],
-            "all_exports" : [],
-            "properties" : {
-            "file_type" : type
-        }
-        };
+            {
+                "all_functions" : [],
+                "all_requires" : [],
+                "all_exports" : [],
+                "properties" : {
+                    "file_type" : type
+                }
+            };
     };
 }
 
@@ -35,11 +38,11 @@ function storeAllFilesAndFunctions(stdout) {
         // console.log("Line: %s", element);
         var array = element.split(':'),
             file = array[0], func = array[1];
-        file1 = file.replace(/\//gi,"_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
+        file1 = cleanPath(file);
         func1 = func.slice(0, func.indexOf("(")).replace(/^function /, "");
 
         //addNewFileModule(file1, "internal");
-	var name = verifyAndAddRequireModule(file1);
+        var name = verifyAndAddRequireModule(file1);
 
         BIM.files[name].all_functions.push(func1);
     });
@@ -57,19 +60,19 @@ function storeAllFilesAndExports(stdout) {
     // ../TSR/racoam/src/ChecksumError.js:module.exports = ChecksumError;
 
     stdout.split("\n").slice(0, -1).forEach(function(element) {
-            var element_array = element.split(':'),
-                file = element_array[0],
-                func = element_array[1];
-            file_name = file.replace(/\//gi, "_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/, "");
-            function_name = func.split('exports');
-            aux = function_name[1].split('=');
-            export_name = aux[0].replace(/\s/g, '').replace(/\./gi, "");
+        var element_array = element.split(':'),
+            file = element_array[0],
+            func = element_array[1];
+        file_name = cleanPath(file);
+        function_name = func.split('exports');
+        aux = function_name[1].split('=');
+        export_name = aux[0].replace(/\s/g, '').replace(/\./gi, "");
 
 
-           // addNewFileModule(file_name, "internal");
-	    var name = verifyAndAddRequireModule(file1);
+        // addNewFileModule(file_name, "internal");
+        var name = verifyAndAddRequireModule(file1);
 
-            BIM.files[name].all_exports.push(export_name);
+        BIM.files[name].all_exports.push(export_name);
     });
 };
 
@@ -82,7 +85,7 @@ function storeAllFilesAndRequires(stdout) {
 
         file = array[0];
         nameOfRequire = array[1] + ((array[2]===undefined)?"":array[2]);
-        file1 = file.replace(/\//gi,"_").replace(/^\.\./gi, "").replace(/\.js$/gi, "").replace(/^_/,"");
+        file1 = cleanPath(file);
 
         //example lines:
         //var btsUri = require("@rcp/bts-uri-utils");
@@ -93,11 +96,12 @@ function storeAllFilesAndRequires(stdout) {
         //console.log(">>>>nameOfRequire: \"%s\", index: %d, length: %d!", nameOfRequire, nameOfRequire.indexOf("require(\""), nameOfRequire.length);
         nameOfRequire1 = nameOfRequire.slice(nameOfRequire.indexOf("require(\"") + 9, nameOfRequire.length);
         nameOfRequire2 = nameOfRequire1.slice(0, nameOfRequire1.indexOf("\""));
-	nameOfRequire2 = nameOfRequire2.replace(/\.js/, "");
+        nameOfRequire2 = nameOfRequire2.replace(/\.js/, "");
+        //nameOfRequire2 = cleanPath(nameOfRequire2);
         //console.log("File is: %s, require is: 0: %s. 1: %s, 2:%s!", file1, nameOfRequire, nameOfRequire1, nameOfRequire2);
 
         //addNewFileModule(file1, "internal");
-	var fileName = verifyAndAddRequireModule(file1);
+        var fileName = verifyAndAddRequireModule(file1);
 
         var nameOfVarRequire = "?undefined?";
         if (nameOfRequire.indexOf("var ") > -1) {
@@ -119,7 +123,7 @@ function storeAllFilesAndRequires(stdout) {
 }
 
 function extendName(module) {
-	return BIM.path2 + module;
+    return BIM.path2 + module;
 }
 
 function verifyAndAddRequireModule(module) {
@@ -130,7 +134,7 @@ function verifyAndAddRequireModule(module) {
         //project nokia external module
 
         console.log("Found nokia external module: %s", module);
-	module = module.replace(/\//g, "_");
+        module = module.replace(/\//g, "_");
         addNewFileModule(module, "external");
 
     } else {
@@ -154,7 +158,8 @@ function verifyAndAddRequireModule(module) {
 }
 
 function transformRelativeNameToAbsoluteName(name) {
-	console.log("999 %s", name);
+    console.log("9999 %s", name);
+    console.log("998 %s %s", name, name.slice(name.lastIndexOf("\.") + 1, name.length).replace(/\//gi, "_"));
     //name = name.replace(/\.js/, "");
     return name.slice(name.lastIndexOf("\.") + 1, name.length).replace(/\//gi, "_");
 }
@@ -172,22 +177,22 @@ function getModuleCompleteName(module) {
 
     Object.keys(BIM.files).some(function(el) {
         //console.log("Checking if: %s can be found in: %s!", newName, el);
-	if (el.indexOf(newName)>-1) {
-		toReturn = el;
-		return true;
-	} else {
-		return false;
-	}
-    }); 
+        if (el.indexOf(newName)>-1) {
+            toReturn = el;
+            return true;
+        } else {
+            return false;
+        }
+    });
 
     if (toReturn.indexOf("\_") === 0) {
 
-    	console.log ("200 getModuleCompleteName for %s is %s", module, toReturn);
+        console.log ("200 getModuleCompleteName for %s is %s", module, toReturn);
 
-    	if(toReturn.indexOf(BIM.path2) === -1 ){
-        	toReturn = extendName(toReturn);
-        	console.log ("220 getModuleCompleteName for %s is %s", module, toReturn);
-    	}
+        if(toReturn.indexOf(BIM.path2) === -1 ){
+            toReturn = extendName(toReturn);
+            console.log ("220 getModuleCompleteName for %s is %s", module, toReturn);
+        }
     }
 
     console.log ("223 getModuleCompleteName for %s is %s", module, toReturn);
@@ -206,10 +211,11 @@ function generateResults_oneAllRequiresUmlFile() {
     var puml_code = "@startuml\n" + "\n";
 
     puml_code = puml_code + "package Internal_modules \{\n"
+
     Object.keys(BIM.files).filter(function(element){
         return BIM.files[element].properties.file_type.indexOf('internal') == 0;
     }).forEach(function(module_name){
-	puml_code = puml_code + "\[" + module_name + "\]" + "\n";
+        puml_code = puml_code + "\[" + module_name + "\]" + "\n";
     });
     puml_code = puml_code + "\}\n\n";
 
@@ -217,7 +223,7 @@ function generateResults_oneAllRequiresUmlFile() {
     Object.keys(BIM.files).filter(function(element){
         return BIM.files[element].properties.file_type.indexOf('node_external') == 0;
     }).forEach(function(module_name){
-	puml_code = puml_code + "\[" + module_name + "\]" + "\n";
+        puml_code = puml_code + "\[" + module_name + "\]" + "\n";
     });
     puml_code = puml_code + "\}\n\n";
 
@@ -225,7 +231,7 @@ function generateResults_oneAllRequiresUmlFile() {
     Object.keys(BIM.files).filter(function(element){
         return BIM.files[element].properties.file_type.indexOf('external') == 0;
     }).forEach(function(module_name){
-	puml_code = puml_code + "\[" + module_name + "\]" + "\n";
+        puml_code = puml_code + "\[" + module_name + "\]" + "\n";
     });
     puml_code = puml_code + "\}\n\n";
 
@@ -234,9 +240,9 @@ function generateResults_oneAllRequiresUmlFile() {
     }).forEach(function(module_name){
         //	console.log("111 Found for module %s %j", module_name,  BIM.files[module_name].all_requires);
         BIM.files[module_name].all_requires.forEach(function(req) {
-        	console.log("111 Found for module %s require %j", module_name,  req.absoluteNameOfRequire);
-        	puml_code = puml_code + "\[" + module_name + "\]-->\[" + req.absoluteNameOfRequire +"\]\n";
-	});
+            console.log("111 Found for module %s require %j", module_name,  req.absoluteNameOfRequire);
+            puml_code = puml_code + "\[" + module_name + "\]-->\[" + req.absoluteNameOfRequire +"\]\n";
+        });
     });
 
 
@@ -306,22 +312,22 @@ function main (){
     }
     var param = process.argv[2];
     BIM.path = param;
-    BIM.path2 = BIM.path.replace(/\//gi, "_").replace(/^\./gi, "").replace(/^\./gi, "").replace(/_$/gi, "").replace(/^_/, "");
+    BIM.path2 = cleanPath(BIM.path);
 
-    exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"^function \"")
+    exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"^function \"", {maxBuffer: 1024 * 1024 *500})
         .then(function (result) {
             var stdout = result.stdout;
             // console.log(stdout);
             storeAllFilesAndFunctions(stdout);
         })
         .then(function () {
-            return exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"require(\" | grep -iv \"return require\"");
+            return exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"require(\" | grep -iv \"return require\"", {maxBuffer: 1024 * 1024 *500});
         })
         .then(function (result) {
             var stdout = result.stdout;
             // console.log(stdout);
             storeAllFilesAndRequires(stdout);
-            return exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"module.exports.\" | grep -iv \"forTests\"");
+            return exec("find " + BIM.path + " -type f -name \"*.js\" | xargs grep -i \"module.exports.\" | grep -iv \"forTests\"", {maxBuffer: 1024 * 1024 *500});
         })
         .then(function(result){
             var stdout = result.stdout;

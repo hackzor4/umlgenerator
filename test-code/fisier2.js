c5b47a51cc48cd14ab1bcd82c5e454d94dc671b3
Alexandru IANAS <alexandru.ianas@nokia.com>
2018-05-10 10:45:48 +0300

    Added test-code

---
+++ test-code/fisier2.js
@@ -0,0 +1,32 @@
+var fisier3 = require('fisier3.js');
+
+function functie21(){
+
+}
+
+function functie22(){
+
+}
+
+function functie23(){
+
+}
+
+function functie24(){
+
+}
+
+function functie25(){
+
+}
+
+function functie26(){
+
+}
+
+module.exports.functie21 = functie21;
+module.exports.functie22 = functie22;
+module.exports.functie23 = functie23;
+module.exports.functie24 = functie24;
+module.exports.functie25 = functie25;
+module.exports.functie26 = functie26;

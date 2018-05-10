f00e4a1cd1c34e2d282db9ac8d58d3db1b30a5a4
Alexandru IANAS <alexandru.ianas@nokia.com>
2018-05-10 11:11:15 +0300

    Renamed test-code to testcode

---
+++ testcode/fisier1.js
@@ -0,0 +1,34 @@
+var fisier2 = require('fisier2');
+
+
+
+function functie1(){
+
+}
+
+function functie2(){
+
+}
+
+function functie3(){
+
+}
+
+function functie4(){
+
+}
+
+function functie5(){
+
+}
+
+function functie6(){
+
+}
+
+module.exports.functie1 = functie1;
+module.exports.functie2 = functie2;
+module.exports.functie3 = functie3;
+module.exports.functie4 = functie4;
+module.exports.functie5 = functie5;
+module.exports.functie6 = functie6;
---
+++ testcode/fisier3.js
@@ -0,0 +1,32 @@
+var fisier1 = require('fisier1.js');
+
+function functie31(){
+
+}
+
+function functie32(){
+
+}
+
+function functie33(){
+
+}
+
+function functie34(){
+
+}
+
+function functie35(){
+
+}
+
+function functie36(){
+
+}
+
+module.exports.functie31 = functie31;
+module.exports.functie32 = functie32;
+module.exports.functie33 = functie33;
+module.exports.functie34 = functie34;
+module.exports.functie35 = functie35;
+module.exports.functie36 = functie36;

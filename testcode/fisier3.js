9c56b02f988f550909fe2589c383cf78a1471277
Alexandru <ianas.alexandru@gmail.com>
2019-10-15 00:56:16 +0300

    testing

--- testcode/fisier3.js
+++ testcode/fisier3.js
@@ -3,29 +3,19 @@ var fisier14 = require("fisier14");
 var fisier8 = require("fisier8");
 var fisier10 = require("fisier10");
 
-function functie31(){
-
+function functie31() {
+  // testing
 }
 
-function functie32(){
+function functie32() {}
 
-}
+function functie33() {}
 
-function functie33(){
+function functie34() {}
 
-}
+function functie35() {}
 
-function functie34(){
-
-}
-
-function functie35(){
-
-}
-
-function functie36(){
-
-}
+function functie36() {}
 
 module.exports.functie31 = functie31;
 module.exports.functie32 = functie32;
